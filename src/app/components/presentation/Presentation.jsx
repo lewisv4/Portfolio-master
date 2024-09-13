d22e620f5dcdaf804886c5f8c95180552724ba4c
lewisv4 <lewisv4re@gmail.com>
2024-09-13 02:33:43 -0500

    added numbers

--- src/app/components/presentation/Presentation.jsx
+++ src/app/components/presentation/Presentation.jsx
@@ -54,15 +54,15 @@ const Presentation = () => {
                             <Link href='https://api.whatsapp.com/send?phone=+573122401377' target='_BLANK' >
                                 <i className="fa-brands fa-whatsapp links "></i><span className='textRed' >+57 312 240 1377</span>
                             </Link>
-                        </button>
-                        <button>
+                    </button>
+                    <button>
                             <Link href='https://api.whatsapp.com/send?phone=+573502032217' target='_BLANK' >
                                 <i className="fa-brands fa-whatsapp links "></i><span className='textRed' >+57 350 203 2217</span>
                             </Link>
-                        </button>
+                    </button>
                     
                     <div className='openAndRedes' >
-                        <h6>{openToWork}</h6>
+                        <h5>{openToWork}</h5>
                         <button>
                             <Link href='https://api.whatsapp.com/send?phone=+573122401377' target='_BLANK' >
                                 <i className="fa-brands fa-whatsapp links "></i><span className='textRed' >+57 312 240 1377</span>
