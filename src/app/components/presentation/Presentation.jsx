2d6ec41b77f5a6e91358f73a15637282237e7e95
lewisv4 <lewisv4re@gmail.com>
2024-09-13 00:34:28 -0500

    added numbers

--- src/app/components/presentation/Presentation.jsx
+++ src/app/components/presentation/Presentation.jsx
@@ -50,11 +50,19 @@ const Presentation = () => {
                     <h1><span className='mensajeSaludo' >{saludo}</span><br />Luis Carlos Rodriguez</h1>
                     <h2>{text}</h2>
                     <p>{parrafo}</p>
-                    <p>+57 3502032217</p>
-                    <p>+57 3122401377</p>
                     
                     <div className='openAndRedes' >
                         <h6>{openToWork}</h6>
+                        <button>
+                            <Link href='https://api.whatsapp.com/send?phone=+573122401377' target='_BLANK' >
+                                <i className="fa-brands fa-whatsapp links "></i><span className='textRed' >+57 312 240 1377</span>
+                            </Link>
+                        </button>
+                        <button>
+                            <Link href='https://api.whatsapp.com/send?phone=+573502032217' target='_BLANK' >
+                                <i className="fa-brands fa-whatsapp links "></i><span className='textRed' >+57 350 203 2217</span>
+                            </Link>
+                        </button>
                         <button>
                             <Link href='https://www.linkedin.com/in/luis-carlos-rodriguez-a885a61a6/' target='_BLANK' >
                                 <i className="fa-brands fa-linkedin links "></i><span className='textRed' >Linkedin</span>
