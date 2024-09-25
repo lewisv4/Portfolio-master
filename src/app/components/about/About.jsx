9ec56d2c6f5662ec1dbd56b7281cc472d1b6d4b1
lewisv4 <lewisv4re@gmail.com>
2024-09-25 00:17:58 -0500

    update

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -44,16 +44,19 @@ const About = () => {
     
     const acerca = currentLenguaje === "es" ? "Acerca de mí" : "About me";
     const parrafo1 = currentLenguaje === "es"
-        ? `Soy un chico apasionado del desarrollo de ${validarEdad(fechaNacimiento)} años de edad con incesante interés y curiosidad por este hermoso mundo, siempre queriendo adquirir nuevos conocimientos y habilidades que me potencien en mi carrera.`
-        : `I'm a passionate boy of ${validarEdad(fechaNacimiento)} years of age with incessant interest and curiosity for this beautiful world, always wanting to acquire new knowledge and skills to enhance my career.`;
+
+    ? `Soy un apasionado desarrollador de ${validarEdad(fechaNacimiento)} años, con un profundo interés y curiosidad por el fascinante mundo del desarrollo de software. Siempre busco adquirir nuevos conocimientos y habilidades que me impulsen en mi carrera. A lo largo de mi trayectoria, he desarrollado una sólida base en el ámbito FullStack, contribuyendo en equipos de trabajo y creando soluciones creativas y efectivas.
+He participado en diversas disciplinas y actividades que han fortalecido mis cualidades personales, como la disciplina, la constancia y la dedicación para alcanzar mis objetivos. Con un enfoque constante en la superación, estoy decidido a expandir mis horizontes y seguir creciendo en el apasionante mundo del desarrollo.`
+        : `I am a passionate developer of ${validateAge(dateBirth)} years, with a deep interest and curiosity for the fascinating world of software development. I am always looking to acquire new knowledge and skills that will propel me in my career. Throughout my career, I have developed a solid foundation in the FullStack field, contributing to work teams and creating creative and effective solutions.
+I have participated in various disciplines and activities that have strengthened my personal qualities, such as discipline, perseverance and dedication to achieve my goals. With a constant focus on self-improvement, I am determined to expand my horizons and continue growing in the exciting world of development.`;
     
     const parrafo2 = currentLenguaje === "es"
-        ? "En mi camino de aprendizaje, pude adquirir conocimientos sólidos en el campo, aportando en equipos de trabajo, creando soluciones creativas y consistentes, mayormente orientadas al front-end."
-        : "In my learning journey, I gained solid knowledge in the field, contributing to teamwork by creating creative and consistent solutions, primarily focused on the front-end.";
+        ? "Mi trayectoria no solo se ha centrado en la técnica; he cultivado habilidades personales como la disciplina, la constancia y la dedicación, que son fundamentales para alcanzar cualquier objetivo. Estoy comprometido con la excelencia y siempre busco superar mis propios límites. Mi meta es expandir mis horizontes y contribuir a proyectos que no solo desafíen mis capacidades, sino que también impacten positivamente en la comunidad tecnológica."
+        : "My career has not only focused on technique; I have cultivated personal skills such as discipline, perseverance and dedication, which are essential to achieve any goal. I am committed to excellence and always seek to surpass my own limits. My goal is to expand my horizons and contribute to projects that not only challenge my capabilities, but also positively impact the technology community.";
     
     const parrafo3 = currentLenguaje === "es"
-        ? "A lo largo de mi vida he llevado a cabo disciplinas y actividades que fortalecieron muchos de mis aspectos personales, como la disciplina, la constancia y la dedicación para cumplir cualquier objetivo."
-        : "Throughout my life, I have engaged in disciplines and activities that strengthened many aspects of my personality, such as discipline, perseverance, and dedication to achieving any goal.";
+        ? "Estoy emocionado por las oportunidades que se avecinan y por seguir creciendo en este apasionante viaje."
+        : "I am excited about the opportunities ahead and continuing to grow on this exciting journey.";
     
     const educacion = currentLenguaje === "es" ? "Educación" : "Education";
     const tecnologias = currentLenguaje === "es" ? "Tecnologías" : "Technologies";
